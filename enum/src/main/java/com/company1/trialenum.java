effcd4aedaa14219cdfc4cf747a5dfe6761fabba
Parul Khanna <parulkhanna14@gmail.com>
2019-12-10 12:43:39 +0530

    enum with logging!

--- enum/src/main/java/com/company1/trialenum.java
+++ enum/src/main/java/com/company1/trialenum.java
@@ -1,5 +1,10 @@
 package com.company1;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.LoggerFactory;
+
 enum Seasons implements enuminterface{
     WINTER(1),
     SUMMER(2),
@@ -18,12 +23,19 @@ enum Seasons implements enuminterface{
 
 public class trialenum {
 
+    static Logger log = Logger.getLogger(trialenum.class);
     public static void main(String[] args) {
+        String log4jConfPath = "/home/sciera/gitprojects/Internship/enum/src/log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
+        //BasicConfigurator.configure();
         for(Seasons s:Seasons.values())
             System.out.println(s);
         Seasons s1=Seasons.FALL;
         int l=s1.getlevel("WINTER");
         System.out.println("Level of WINTER: "+l);
         System.out.println("ORDER OF FALL:: "+Seasons.valueOf("FALL").ordinal());
+        log.info("Enum code running!");
+        log.debug("This is debug log!!");
+        log.warn("Warning!");
     }
 }
