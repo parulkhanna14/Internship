2bababe73158ab34d6c5044137d3a8ea0ca41f7f
Parul Khanna <parulkhanna14@gmail.com>
2019-12-13 12:53:55 +0530

    login with tomcat+logging

--- login/src/main/java/Login.java
+++ login/src/main/java/Login.java
@@ -3,17 +3,22 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
 import java.io.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Login extends HttpServlet {
 
+    static Logger log=Logger.getLogger(Login.class);
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-
+        String log4jConfigFile = "/home/sciera/gitprojects/Internship/login/src/Log4j.properties";
+        PropertyConfigurator.configure(log4jConfigFile);
         String email = request.getParameter("email");
         String pass = request.getParameter("pass");
-        out.println(email);
-        out.println(pass);
+        /*out.println(email);
+        out.println(pass);*/
         if(Validate.checkUser(email, pass))
         {
             RequestDispatcher rs = request.getRequestDispatcher("Welcome");
@@ -21,6 +26,7 @@ public class Login extends HttpServlet {
         }
         else
         {
+            log.error("Username or Password incorrect with email as: "+email+" and password as:  "+pass);
             out.println("Username or Password incorrect");
             RequestDispatcher rs = request.getRequestDispatcher("index.jsp");
             rs.include(request, response);
--- login/src/main/java/Validate.java
+++ login/src/main/java/Validate.java
@@ -1,16 +1,22 @@
+import java.io.FileReader;
 import java.sql.*;
+import java.util.Properties;
 
 public class Validate {
     public static boolean checkUser(String email,String pass)
     {
         boolean st =false;
         try {
-
+            FileReader fileReader=new FileReader("/home/sciera/gitprojects/Internship/login/src/main/resources/info.properties");
+            Properties p=new Properties();
+            p.load(fileReader);
             //loading drivers for mysql
             Class.forName("com.mysql.jdbc.Driver");
 
             //creating connection with the database
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db","parul","Kritter12!");
+            //Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db","parul","Kritter12!");
+            String url="jdbc:mysql://"+p.getProperty("hostname")+":"+p.getProperty("port")+"/"+p.getProperty("database");
+            Connection con= DriverManager.getConnection(url,p.getProperty("uname"),p.getProperty("passwd"));
             PreparedStatement ps = con.prepareStatement("select * from Student where email=? and pass=?");
             ps.setString(1, email);
             ps.setString(2, pass);
